744b8d9cc43f97cfa23c7c06b6778b0cad297475
mohasal0101 <mohasal0101@gmail.com>
2022-07-24 15:52:31 +0300

    Unicorns-react

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './header.js';
+import Footer from './footer.js';
+import Main from './main.js';
+
+function App() {
+  return (
+    <div className="App">
+       <Header/>
+       <Main/>
+       <Footer/>
+    </div>
+  );
+}
+
+export default App;
