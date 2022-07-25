c2ac23c8150ffeee9d6666b449fa6fcfe42b7f94
mohasal0101 <mohasal0101@gmail.com>
2022-07-25 23:51:17 +0300

    hornedBeast-update

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Footer.css';
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer >
+        all rights reserved &copy; Salah Al-Hamawi
+      </footer>
+    );
+  }
+}
+
+export default Footer;
---
+++ src/components/SelectedBeast.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Card, Modal, Button } from 'react-bootstrap';
+
+
+class SelectedBeast extends React.Component {
+  render() {
+    return (
+      <>
+        <Modal  show={this.props.showData} onHide={this.props.handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>{this.props.modalData.title}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+
+            <Card>
+              <Card.Img src={this.props.modalData.img_source}
+                alt={this.props.modalData.title}
+              />
+              <Card.Body>
+                <Card.Text className="descriptionText">
+                  {this.props.modalData.description}
+                </Card.Text>
+              </Card.Body>
+            </Card>
+
+
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={this.props.handleClose}>
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </>
+    );
+  }
+}
+
+export default SelectedBeast;
